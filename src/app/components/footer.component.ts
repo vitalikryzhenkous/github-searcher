303db09c6afbe2605c62f01bda3282ad71b471b9
vitaliiryzhenkous <vitaliiryzhenkous@gmail.com>
2017-08-31 17:22:55 +0300

    Auto-generated commit

--- src/app/components/footer.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-    selector: 'app-footer',
-    template: `
-        <footer></footer>
-    `,
-    styles: [``]
-})
-export class FooterComponent {}
--- src/app/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './header.component';
-export * from './main.component';
-export * from './footer.component';
